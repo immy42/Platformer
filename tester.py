6c468fab8ba1b4910a72ff27ff9440af9f3a2121
RG <xretrogamerz@gmail.com>
2022-11-27 17:04:04 +0000

    v9.3.4

--- tester.py
+++ tester.py
@@ -1,12 +1,2 @@
-class enemy:
-    def __init__(self):
-        self.HP = 10
-
-class enemyName:
-    def __init__(self):
-        self.par = enemy()
-    def update(self):
-        print(self.par.HP)
-
-eN = enemyName()
-eN.update()
+import pip
+pip.main(['install','pillow'])
